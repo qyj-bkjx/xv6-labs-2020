ebf11833151fc55f973e32dfe74042c1d8a45694
zjs <1076672244@qq.com>
2024-01-18 20:25:58 +0800

    ready to submit my lab3-1

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,27 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+//heplerfunction for vmprint
+void helpervmprint(pagetable_t pagetable, int level){
+  if (level > 2)
+    return;
+  for (int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if ((pte & PTE_V)){
+      //this pte pointer to a lower-level page table
+      uint64 child = PTE2PA(pte);
+      for (int j = 0; j <= level; j++){
+        printf("..");
+        if (j != level)
+          printf(" ");
+      }
+      printf("%d: pte %p pa %p\n", i, pte, child);
+      helpervmprint((pagetable_t)child, level + 1);
+    }
+  }
+}
+//function to help print the contents of a page table
+void vmprint(pagetable_t pagetable){
+  printf("page table %p\n", pagetable);
+  helpervmprint(pagetable, 0);
+}
